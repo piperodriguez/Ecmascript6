7efca82df37ed7875a10e0838e8d6fd04cc08f29
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2020-01-26 13:02:52 -0500

    Declaracion de variables con LET y Const

--- Project/output/constLet.js
+++ Project/output/constLet.js
@@ -1,9 +1,44 @@
 "use strict";
 
-//Variable declaracion ecmascript			
-var nombre = "Juan Felipe";
-var apellido = "Rodriguez Vargas";
-var edad = 23;
-/**Declaracion de variables */
+/*//Variable declaracion ecmascript			
+let nombre = "Juan Felipe";
+let apellido = "Rodriguez Vargas";
+let edad = 23;
 
-console.log("Hello " + nombre + " tu apellido es " + apellido + " ?");
+
+console.log(`¡Hello ${nombre}! your last name is ${apellido} ?`);
+
+/**Declaracion de variables */
+//mala practica logica permitir redeclarar el valor de la variable
+
+/*var sexo = "Femenino";
+var sexo = "Masculino";
+
+console.log(`¡tu sexo es ${sexo}!`);
+
+//remplza el valor original
+
+//LET
+// no permite redeclarar el valor de la variable respetando su valor inicial.
+
+//SCOP DE JAVASCRIPT -> lo poseemos protege la variable
+let pais = "Unitated States";
+let pais = "Venezuela";
+console.log(`¡Welcome to ${pais}!`);
+*/
+//LET aplicando scop
+function saludo() {
+  var nombre = 'Juan Felipe';
+  return "\xA1Hola ".concat(nombre, "!");
+} //para ejecutar esta funcion
+
+
+console.log(saludo());
+/*Ee¡jemplo uso variable let*/
+
+var edad = 18;
+
+if (edad >= 18) {
+  var requisitoEdad = true;
+  console.log("La persona es mayor de edad ".concat(requisitoEdad));
+}
--- Project/src/constLet.js
+++ Project/src/constLet.js
@@ -1,7 +1,43 @@
-//Variable declaracion ecmascript			
+/*//Variable declaracion ecmascript			
 let nombre = "Juan Felipe";
 let apellido = "Rodriguez Vargas";
 let edad = 23;
 
+
+console.log(`¡Hello ${nombre}! your last name is ${apellido} ?`);
+
 /**Declaracion de variables */
-console.log("Hello "+nombre+" tu apellido es "+apellido+" ?");
+//mala practica logica permitir redeclarar el valor de la variable
+
+/*var sexo = "Femenino";
+var sexo = "Masculino";
+
+console.log(`¡tu sexo es ${sexo}!`);
+
+//remplza el valor original
+
+//LET
+// no permite redeclarar el valor de la variable respetando su valor inicial.
+
+//SCOP DE JAVASCRIPT -> lo poseemos protege la variable
+let pais = "Unitated States";
+let pais = "Venezuela";
+console.log(`¡Welcome to ${pais}!`);
+*/
+//LET aplicando scop
+function saludo(){
+    var nombre = 'Juan Felipe';
+    return `¡Hola ${nombre}!`;
+}
+
+//para ejecutar esta funcion
+console.log(saludo());
+/*Ee¡jemplo uso variable let*/
+
+let edad = 18;
+
+if (edad >= 18) {
+    let requisitoEdad = true;
+    console.log(`La persona es mayor de edad ${requisitoEdad}`);
+}
+
