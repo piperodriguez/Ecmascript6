713429849c8678c240ba4cce7afc2e0268e20b8a
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2020-01-25 23:20:41 -0500

    Declaracion de Variables Let Const

---
+++ Project/output/constLet.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Variable declaracion ecmascript			
+var nombre = "Juan Felipe";
+var apellido = "Rodriguez Vargas";
+var edad = 23;
+/**Declaracion de variables */
+
+console.log("Hello " + nombre + " tu apellido es " + apellido + " ?");
---
+++ Project/src/constLet.js
@@ -0,0 +1,7 @@
+//Variable declaracion ecmascript			
+let nombre = "Juan Felipe";
+let apellido = "Rodriguez Vargas";
+let edad = 23;
+
+/**Declaracion de variables */
+console.log("Hello "+nombre+" tu apellido es "+apellido+" ?");
